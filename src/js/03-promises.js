18533f73516e3f2249e8554157b4b2be8d5ecc3b
Oleynik Konstantin <oleynik.kostia2@gmail.com>
2022-09-21 18:06:12 +0300

    finush

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const refs = {
+  formEl: document.querySelector('.form'),
+  delayEL: document.querySelector('input[name="delay"]'),
+  stepEl: document.querySelector('input[name="step"]'),
+  amountEl: document.querySelector('input[name="amount"]'),
+};
+
+refs.formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  let { delay, step, amount } = onGettingData();
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(()=> {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onGettingData() {
+  return {
+    delay: Number(refs.delayEL.value),
+    step: Number(refs.stepEl.value),
+    amount: Number(refs.amountEl.value),
+  };
 }
